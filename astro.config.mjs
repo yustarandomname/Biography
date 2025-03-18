d02dbc47dc5dc03bf8d7ab29819fcae608320400
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-03-18 09:52:57 +0100

    Add utility functions, button component, and update index page layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-
 import vercel from '@astrojs/vercel';
+import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   adapter: vercel(),
 
   vite: {
