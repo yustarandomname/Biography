ee1b1ca5a9a6986f12888d4460f5ab06321f7341
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-03-18 09:29:24 +0100

    setup

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import vercel from '@astrojs/vercel';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
