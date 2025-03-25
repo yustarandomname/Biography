2faedb5155f5cfe5e1f17c24d5f6151ad51d1dd4
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-03-25 17:35:21 +0100

    Added better styling

---
+++ src/components/paper/index.ts
@@ -0,0 +1,6 @@
+import Bibliography from "./Bibliography.astro";
+import Cite from "./Cite.astro";
+import CiteList from "./CiteList.astro";
+import Paper from "./Paper.astro";
+
+export { Bibliography, Cite, CiteList, Paper };
--- src/components/svelte/bib/BibState.svelte.ts
+++ src/components/svelte/bib/BibState.svelte.ts
@@ -1,9 +1,64 @@
 import { SvelteMap } from "svelte/reactivity";
 
-export type Paper = {
-  id: string;
+type Base = {
+  ID: string;
+  author: string;
+  authors: string[];
+  abstract?: string;
   title: string;
+  url?: string;
+  doi?: string;
+  year: string;
+  month?: string;
 };
+
+interface ResearachPaper extends Base {
+  ENTRYTYPE: "researchpaper";
+  title: string;
+}
+
+interface TechReport extends Base {
+  ENTRYTYPE: "techreport";
+  institution: string;
+  type: string;
+  bibsource: string;
+}
+
+interface Journal extends Base {
+  ENTRYTYPE: "journal" | "inproceedings" | "article";
+  issn?: string;
+  isbn?: string;
+  journal: string;
+  pages?: string;
+  volume?: string;
+  number?: string;
+  issue?: string;
+  eprint?: string;
+  editor?: string;
+}
+
+interface Book extends Base {
+  ENTRYTYPE: "book";
+  keywords?: string;
+  note?: string;
+  publisher: string;
+  language?: string;
+  pages?: `${number}-${number}`;
+  volume?: `${number}`;
+}
+
+interface WebPage extends Base {
+  ENTRYTYPE: "webpage";
+  author: string;
+  authors: string[];
+  url: string;
+  title: string;
+  year: string;
+  month?: string;
+  journal?: string;
+}
+
+export type Paper = ResearachPaper | Journal | Book | WebPage;
 
 class Refs {
   papers = new SvelteMap<string, Paper>();
@@ -13,7 +68,7 @@ class Refs {
   }
 
   addPaper(paper: Paper) {
-    this.papers.set(paper.id, paper);
+    this.papers.set(paper.ID, paper);
   }
 }
 
