719f290a1a87bfed1ba2b901239e65b898884098
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-03-31 20:58:11 +0200

    Added method to bachelor thesis blog

--- src/components/paper/index.ts
+++ src/components/paper/index.ts
@@ -3,5 +3,6 @@ import Bibliography from "./Bibliography.astro";
 import Cite from "./Cite.astro";
 import CiteList from "./CiteList.astro";
 import Label from "./Label.astro";
+import Equation from "./Equation.astro";
 
-export { AutoRef, Bibliography, Cite, CiteList, Label };
+export { AutoRef, Bibliography, Cite, CiteList, Label, Equation };
